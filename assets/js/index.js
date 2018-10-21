ca091118ef4bc50adf4d301720f819198d300cc7
Nenivar <nenivar@gmail.com>
2018-10-21 13:52:14 +0100

    back of cover hidden

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
-function bookOnClick(ele) {
+function coverOnClick(ele) {
     ele.classList.toggle("opening");
 }
+
+/* var cover = document.querySelector('.cover');
+cover.addEventListener('click', function() {
+    cover.classList.toggle('opening');
+}); */
+
