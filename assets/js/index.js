88e76fad160ae1ea3d4d1b39933872f1bec10c41
Nenivar <nenivar@gmail.com>
2018-10-21 17:22:19 +0100

    tried to make pages move without success

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
 function coverOnClick(ele) {
-    ele.classList.toggle("opening");
+    ele.classList.toggle('opening');
+
+    x = document.getElementsByClassName('middlepaper');
+
+    for (let e of x) {
+        paperOnClick(e);
+    }
+}
+
+function paperOnClick(ele) {
+    ele.classList.toggle('opening');
+
+    ele.style.zIndex =
+        (ele.classList.contains('opening')) ? 10 : 0;
 }
 
 /* var cover = document.querySelector('.cover');
