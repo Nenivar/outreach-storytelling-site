1a80e3163a9026daa2c6748bd674c16fb8b3e111
Nenivar <nenivar@gmail.com>
2018-11-19 01:37:23 +0000

    nicer syntax highlighting

---
+++ assets/js/post.js
@@ -0,0 +1,11 @@
+/* this is pretty horrible */
+/* initialY = 48;
+sidebar = document.getElementsByClassName('sidebar');
+
+window.addEventListener('scroll', function(e) {
+    Array.prototype.forEach.call(sidebar, function(ele) {
+        if (window.scrollY > initialY) {
+            ele.setAttribute("style", "position: fixed; top: 0; height: 100%")
+        }
+    });
+}); */
